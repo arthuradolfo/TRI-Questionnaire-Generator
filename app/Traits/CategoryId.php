decd55cfa889ceffd2a32cf04ee5f97a74a2579f
Arthur <arthur_adolfo@hotmail.com>
2020-10-07 21:49:39 -0300

    chore: Fix code smells

--- app/Traits/CategoryId.php
+++ app/Traits/CategoryId.php
@@ -10,7 +10,10 @@ trait CategoryId
     {
         static::creating(function ($model) {
             $category = Category::findOrFail($model->category_id);
-            if($category->user_id != $model->user_id) abort(401, "Category ID does not exist for this user.");
+            if($category->user_id != $model->user_id)
+            {
+                abort(401, "Category ID does not exist for this user.");
+            }
         });
     }
 }
