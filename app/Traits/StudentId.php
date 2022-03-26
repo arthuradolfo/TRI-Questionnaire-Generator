7f4b7ca29b6f132f28bf43e7508c958d112d5ad8
Arthur Adolfo <arthur.adolfo@sap.com>
2022-03-26 14:39:05 -0300

    chore: Permit student to update Session created by teacher

--- app/Traits/StudentId.php
+++ app/Traits/StudentId.php
@@ -11,12 +11,6 @@ trait StudentId
         static::creating(function ($model) {
             if(!is_null($model->student_id))
             {
-                $student = Student::findOrFail($model->student_id);
-
-                if($student->user_id != $model->user_id)
-                {
-                    abort(401, "Student ID does not exist for this user.");
-                }
             }
         });
     }
